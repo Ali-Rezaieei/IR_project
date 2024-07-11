c38509f2cdd07c423492e5cd12ef39209efd67c7
Ali-Rezaieei <90846267+Ali-Rezaieei@users.noreply.github.com>
2024-07-11 22:51:13 +0330

    Add files via upload

---
+++ indexer.py
@@ -0,0 +1,234 @@
+import json
+from typing import List
+from heapq import heappop, heappush, heapify
+from hazm import Normalizer, Lemmatizer, word_tokenize
+
+# This is the indexer class
+# It handles everything related to indexing!
+class Indexer:
+
+    # Initializer index
+    def __init__(self, load_addr:str, save_addr:str, refined_db_addr:str, remove_x_sw:int, enable_normalizer:bool=True, debug_mode = False) -> None:
+        # Indexer config
+        self.load_addr = load_addr
+        self.save_addr = save_addr
+        self.refined_db_addr = refined_db_addr
+        self.remove_x_sw = remove_x_sw              # remove x most frequent words
+        self.db = None                              # loading db into db
+        self.refined_db = dict()                    # after reading db - we create refined from docs
+        self.index = dict()                         # building index using dict as base data structure
+        self.enable_normalizer = enable_normalizer  # enabling normalizer
+        self.DEBUG = debug_mode         
+        
+        # setup tools here
+        self.normalizer = Normalizer().normalize
+        self.stemmer = Lemmatizer().lemmatize
+        self.tokenizer = word_tokenize
+        self.useless_notations = ['_' ,'-', '+', '*', '%', '$', '/', '.', '!', '(', ')', '^', '=', '<', '>', '?', '&', '@', '\\', ';', '\'', '\"', '{', '}', '[', ']', ':', '«', '»', ','] # useless notations
+        self.stop_words = []
+
+    # Load Database (a json files here)
+    def __load_db(self):
+        try:
+            count = 0
+            with open(self.load_addr, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                self.db = dict()
+                for id in data:
+                    self.db[id] = {
+                        "content" : data[id]['content'],
+                    }
+                    shortened_content = ''
+                    if len(data[id]['content']) > 200:
+                        shortened_content = data[id]['content'][:200] + '...'
+                    else:
+                        shortened_content = data[id]['content']
+                    self.refined_db[id] = {
+                        "title": data[id]['title'],
+                        "content" : shortened_content ,
+                        "url" : data[id]['url'],
+                        "t_count": 0
+                    }
+                    count += 1
+                print('Max Docs:', count)
+                return
+        # File not found
+        except FileNotFoundError:
+            print("file not found in {}".format(self.load_addr))
+        # Invalid json file
+        except json.JSONDecodeError as e:
+            print("JSON decoding error:", e)
+        # Other exceptions
+        except Exception as e:
+            print("error:", e)
+        self.db = None       
+
+    # Save index file
+    def __save_index(self):
+        
+        with open(self.save_addr, 'w', encoding='UTF-8') as file:
+            json.dump(self.index, file, indent=2, ensure_ascii=False)
+
+        with open(self.refined_db_addr, 'w', encoding='UTF-8') as file2:
+            json.dump(self.refined_db, file2,  indent=2 ,ensure_ascii=False)
+
+    # Normalizer is used to process contents and normalizing the text for that content 
+    def __normalizer(self, content:str) -> str:
+        if not self.enable_normalizer: return content
+        content = self.normalizer(content)
+        res = ''
+        for c in content:
+            if c in self.useless_notations:
+                res = res + ' '
+            else:
+                res = res + c
+        return res
+
+    # This function is used for tokenization
+    def __tokenizer(self, content:str)->List[str]:
+        return self.tokenizer(content)
+
+    # This function is used for stemming
+    def __stemmer(self, term:str) -> str:
+        stem = self.stemmer(term)
+        if len(stem) == 0:
+            # return term
+            return ''
+        else: 
+            return stem
+
+    # Use this function to extract you stop word list!
+    # You can disable this feature!
+    def __remove_sw(self):
+        heap = []
+        heapify(heap)
+        for term in list(self.index.keys()):
+            heappush(heap, (-1 * int(self.index[term]['freq']), term))
+
+        max_element = len(heap)
+        if self.DEBUG and self.remove_x_sw >= 10 : print('Here you can see 10 first stop words:')
+        for i in range(self.remove_x_sw):
+            if i >= max_element:
+                return
+            item = heappop(heap)
+            if self.DEBUG and i < 10:
+                print("{}-term: {}, freq: {}".format(i, item[1], item[0] * -1))
+            self.index.pop(item[1])
+
+    # Do indexing operation here
+    def __indexer_engine(self):
+
+        # *********************** This are is for tokenization process:
+        tokens = []
+        show_one_sample = True
+        for id in self.db:
+            # First: Normalize Content
+            if show_one_sample and self.DEBUG:
+                print('First Content Before Normalization:{}'.format(self.db[id]['content']))
+            content = self.__normalizer(self.db[id]['content'])
+            if show_one_sample and self.DEBUG:
+                print('After Normalization: {}'.format(content))
+            # Second: Tokenizing
+            tks = self.__tokenizer(content)
+            if show_one_sample and self.DEBUG:
+                print('After Tokenization: ', tks)
+            new_tokens = []
+            # Third: Stemming the tokens
+            for t in tks:
+                stem = self.__stemmer(t)
+                if stem == '':
+                    continue
+                new_tokens.append(stem)
+
+            if show_one_sample and self.DEBUG:
+                print('After Stemming Terms in Tokens:', new_tokens)
+
+            self.refined_db[id]['t_count'] = len(new_tokens)
+            
+            # Forth: Appending new tokens to the tokens list
+            for item in new_tokens:
+                if item == '':
+                    continue
+                tokens.append({'doc_id': id, 'token':item})
+            
+            if show_one_sample and self.DEBUG:
+                print('Making tokens ready for indexing {term, doc_id}: ', tokens)
+                
+            show_one_sample = False
+            
+        # Test Area
+        if self.DEBUG:
+            if len(tokens) >= 10:
+                print("Some of tokens ready to index:", tokens[:10])
+            else:
+                print("Some of tokens ready to index:", tokens)
+
+        # *********************** end of tokenization
+        
+        # ####################### This are is for indexing process:
+        last_doc_id = '-1'
+        position = 0
+        for item in tokens:
+
+            doc_id = item['doc_id']
+            token = item['token']
+            
+            if last_doc_id != doc_id:
+                position = 0
+                last_doc_id = doc_id
+            
+            position += 1
+            
+            if token in self.index:
+                temp_obj = self.index[token]
+                new_freq = temp_obj['freq'] + 1
+                postings_list = temp_obj['postings_list']
+                if doc_id not in postings_list:
+                    postings_list[doc_id] = {'tf':1, 'positions':[position]}
+                else:
+                    postings_list[doc_id]['tf'] = postings_list[doc_id]['tf'] + 1
+                    pl = postings_list[doc_id]['positions']
+                    pl.append(position)
+                    postings_list[doc_id]['positions'] = pl
+                self.index[token]['freq'] = new_freq
+                self.index[token]['postings_list'] = postings_list
+            else:
+                # we have a term freq - and term/doc freq in postings
+                self.index[token] = {'freq': 1, 'postings_list': {doc_id:{'tf':1, 'positions':[position]}}}
+
+        # sorting dictionary by term
+        self.index = dict(sorted(self.index.items()))
+        for term in self.index:
+            pl = list(self.index[term]['postings_list'].keys())
+            pl_int = [int(x) for x in pl]
+            pl_int.sort()
+            new_pl = dict()
+            for x in pl_int:
+                x_str = str(x)
+                new_pl[x_str] = self.index[term]['postings_list'][x_str]
+            self.index[term]['postings_list'] = new_pl
+            
+                
+
+        # remove X most frequent words
+        if self.remove_x_sw > 0:
+            self.__remove_sw()
+
+
+        
+        if self.DEBUG:
+            print("Number of extracted terms in dict: {}".format(len(list(self.index.keys()))))
+
+        # ####################### end of indexing
+
+    # set normalizer
+    def set_enable_normalizer(self, enable_normalizer:bool=True):
+        self.enable_normalizer = enable_normalizer
+        
+    # Run the indexer
+    def run(self):
+        self.__load_db()
+        if self.db is None: 
+            return
+        self.__indexer_engine()
+        self.__save_index()
---
+++ search_engine.py
@@ -0,0 +1,303 @@
+import json, math, time
+from typing import List
+from hazm import Normalizer, Lemmatizer, word_tokenize
+from heapq import heappop, heappush, heapify
+
+class SearchEngine:
+
+    def __init__(self, index_addr:str, refined_db_addr:str, enable_normalizer:bool=True, debug_mode:bool=False) -> None:
+        # class configs
+        self.index_addr = index_addr
+        self.refined_db_addr = refined_db_addr
+        self.index_is_loaded = False
+        self.index = dict()
+        self.db = dict()
+        self.DEBUG = debug_mode
+        self.max_doc = 1000
+        self.enable_normalizer = enable_normalizer
+        self.using_champions_allowed = False # it is not allowed by default
+        self.champions_list = dict()         # TODO: it goes from a token to a few number of docs
+        self.search_score_mode = 'tf_idf'
+        self.max_display_res = 5
+        # tools configs
+        self.normalizer = Normalizer().normalize
+        self.stemmer = Lemmatizer().lemmatize
+        self.tokenizer = word_tokenize
+        self.useless_notations = ['_' ,'-', '+', '*', '%', '$', '/', '.', '!', '(', ')', '^', '=', '<', '>', '?', '&', '@'
+                                   '\\', ';', '\'', '\"', '{', '}', '[', ']', ':', '«', '»', ','] # useless notations
+
+    # Loads Index File
+    def __load_index(self):
+        try:
+            with open(self.index_addr, 'r', encoding='utf-8') as file:
+                self.index = json.load(file)
+                return True
+        # File not found
+        except FileNotFoundError:
+            print("file not found in '{}'".format(self.index_addr))
+        # Invalid json file
+        except json.JSONDecodeError as e:
+            print("JSON decoding error:", e)
+        # Other exceptions
+        except Exception as e:
+            print("error:", e)
+        return False
+    
+        # Loads Index File
+    
+    # Loads Refined DB
+    def __load_refined_db(self):
+        try:
+            with open(self.refined_db_addr, 'r', encoding='utf-8') as file:
+                self.db = json.load(file)
+                self.max_doc = len(list(self.db.keys()))
+                if self.DEBUG:
+                    print("Max Doc:{}".format(self.max_doc))
+                return True
+        # File not found
+        except FileNotFoundError:
+            print("file not found in '{}'".format(self.refined_db_addr))
+        # Invalid json file
+        except json.JSONDecodeError as e:
+            print("JSON decoding error:", e)
+        # Other exceptions
+        except Exception as e:
+            print("error:", e)
+        return False
+
+    # Pre process queries
+    def __query_processor(self, q:str) -> List[str]:
+        nq = self.normalizer(q) if self.enable_normalizer else q
+        rmun_nq = ''
+        for i in nq:
+            if i in self.useless_notations:
+                rmun_nq = rmun_nq + ' '
+            else:
+                rmun_nq = rmun_nq + i
+        tokens = self.tokenizer(rmun_nq)
+        stemmed_tokens = []
+        for i in range(len(tokens)):
+            stem = self.stemmer(tokens[i])
+            if len(stem) == 0:
+                continue
+            stemmed_tokens.append(stem)
+        if self.DEBUG:
+            print("purified query tokens: ", stemmed_tokens)
+        return stemmed_tokens
+
+    # Displays result of search
+    def __display_results(self, res:List[str]):
+        print("Search Results:")
+        if len(res) == 0:
+            print("no result found!")
+            return
+        count = 0
+        for doc_id in res:
+            if count >= self.max_display_res:
+                return
+            count += 1
+            title = self.db[doc_id]['title']
+            content = self.db[doc_id]['content']
+            if len(content) > 100:
+                content = content[:100] + '...'
+            url = self.db[doc_id]['url']
+            print('{} - Title: {}'.format(count, title))
+            print(content.replace('\n', ' '))
+            print(url.replace('\n', ' '))
+
+    # use this for merging to list and remaining the orders
+    def __merge_postings(self, list1:List[str], list2:List[str]):
+        ans = []
+        if len(list1) != 0: item1 = list1.pop(0)
+        if len(list2) != 0: item2 = list2.pop(0)
+        while len(list1) != 0 and len(list2) != 0:
+            if int(item1) > int(item2):
+                ans.append(item2)
+                item2 = list2.pop(0)
+            elif int(item1) < int(item2):
+                ans.append(item1)
+                item1 = list1.pop(0)
+            else:
+                ans.append(item1)
+                item1 = list1.pop(0)
+                item2 = list2.pop(0)
+        if len(list1) != 0:
+            ans.extend(list1)
+        elif len(list2) != 0:
+            ans.extend(list2)
+        return ans
+    
+    # scoring docs for a term
+    def __score_tf_idf(self, term: str, doc_id: str) -> float:
+        if self.index.get(term) == None:
+            # if self.DEBUG: print("No such term '{}' in index.".format(term))
+            return 0 # no such term exists in our index file
+        if self.index[term]['postings_list'].get(doc_id) == None:
+            # if self.DEBUG: print("No such index={} for term: {}".format(doc_id, term))
+            return 0 # there is no such term in this doc
+        else:
+            tf = math.log10(1 + self.index[term]['postings_list'][doc_id]['tf'])
+        df = len(list(self.index[term]['postings_list'].keys()))
+        idf = math.log10(self.max_doc/df)
+        # if self.DEBUG: print("for term'{}' and doc:{} tf={}, idf={}".format(term, doc_id, tf, idf))
+        return tf*idf
+
+    # scoring docs by cosine, vector based!
+    # alpha is smoother
+    def __score_cosine(self, vector_q:List[float], vector_q_len: float, vector_d:List[float], alpha:float=0.001)->float:
+        vector_d_len = 0.0
+        for i in vector_d:
+            vector_d_len += math.pow(i, 2)
+        vector_d_len = math.sqrt(vector_d_len)
+        score = 0.0
+        for i in range(len(vector_q)):
+            score += (vector_q[i] * vector_d[i])
+        dv = vector_d_len * vector_q_len + alpha 
+        score /= dv
+        return score
+
+    # search and rank here
+    # alpha is smoother
+    def __search(self, processed_q:List[str], score_mode='tf_idf', alpha:float=0.001) -> List[str]:
+        
+        # set search index (?champions)
+        if self.using_champions_allowed:
+            index = self.champions_list
+        else:
+            index = self.index
+        
+        start_time = time.time()
+        
+        # searching only in related docs - index elimination        
+        related_doc_id = []
+        for term in processed_q:
+            if index.get(term) == None:
+                continue
+            related_doc_id = self.__merge_postings(related_doc_id, list(index[term]['postings_list'].keys()))
+        
+        # using max heap to process faster the top k
+        heap = []
+        heapify(heap)
+         
+        # using cosine scoring.
+        if score_mode=='cosine':
+            q_vec = []
+            for term in processed_q:
+                if index.get(term) != None:
+                    idf = math.log(self.max_doc/(1 + len(list(index[term]['postings_list'].keys())))) + alpha
+                else:
+                    idf = alpha
+                q_vec.append(idf)
+            
+            q_vec_len = 0
+            for item in q_vec:
+                q_vec_len += math.pow(item, 2)
+            q_vec_len = math.sqrt(q_vec_len)     
+            
+            for doc_id in related_doc_id:
+                d_vec = []
+                for term in processed_q:
+                    if index.get(term) != None:
+                        if index[term]['postings_list'].get(doc_id) != None:
+                            tf = math.log10(1 + index[term]['postings_list'][doc_id]['tf']) + alpha
+                        else:
+                            tf = alpha
+                    else:
+                        tf = alpha
+                    d_vec.append(tf)
+                score = self.__score_cosine(q_vec, q_vec_len, d_vec, alpha)
+                heappush(heap, (score * -1, doc_id))
+
+        # using tf*idf scoring. DEFAULT
+        else:
+            for doc_id in related_doc_id:
+                score = 0.0
+                for term in processed_q:
+                    score = score + self.__score_tf_idf(term, doc_id)
+                heappush(heap, (score * -1, doc_id))
+        
+        stop_time = time.time()
+        
+        # extracting res
+        res = []
+        count = 0
+        while len(heap) > 0:
+            item = heappop(heap)
+            if self.DEBUG and count < 5:
+                print("{}-Doc_ID: {} - Score: {}".format(count, item[1], item[0] * -1))
+            res.append(item[1])
+            count += 1
+        if self.DEBUG:
+            print("{} results in {} seconds:".format(len(res), stop_time - start_time))
+        return res
+
+    # this function updates champions list, then enables it -> so search engine will use champions list
+    def enable_champions_list(self, x_most_related = 5):
+        for term in self.index:
+            postings = self.index[term]['postings_list']
+            if len(list(postings.keys())) > x_most_related:
+                heap = []
+                heapify(heap)
+                for doc_id in postings:
+                    heappush(heap, (postings[doc_id]['tf'] ,doc_id))
+                extracted_top_docs = list()
+                i = 0
+                while i < x_most_related:
+                    extracted_top_docs.append(int(heappop(heap)[1]))
+                    i += 1
+                # sort in-place
+                extracted_top_docs.sort()
+                new_postings = dict()
+                for doc_id in extracted_top_docs:
+                    new_postings[str(doc_id)] = {'tf':postings[str(doc_id)]['tf'], 'positions': postings[str(doc_id)]['positions']}
+                self.champions_list[term] = {'freq':self.index[term]['freq'],'postings_list':new_postings}    
+            else:
+                self.champions_list[term] = {'freq':self.index[term]['freq'],'postings_list':postings}
+        self.using_champions_allowed = True
+        if self.DEBUG:
+            with open('./debug_champions_list.json', 'w', encoding='UTF-8') as file:
+                json.dump(self.champions_list, file, indent=2, ensure_ascii=False)
+ 
+    # This function prevents search engine from using champions list -> makes it default use of index
+    def disable_champions_list(self):
+        self.using_champions_allowed = False
+    
+    # set score function
+    def set_search_score_mode(self, mode='tf_idf'):
+        self.search_score_mode = mode
+        print('search mode set to \'{}\'. if not exist default is tf_idf'.format(mode))
+    
+    # set max display res
+    def set_max_display_res(self, max_display_res:int):
+        if max_display_res <= 0:
+            self.max_display_res = 1
+        else:
+            self.max_display_res = max_display_res
+    
+    # start search engine
+    def run(self):
+        print("Running search engine...")
+        if not self.__load_index():
+            if self.DEBUG:
+                print("Failed to run search engine.")
+                return
+        self.__load_refined_db()
+        self.index_is_loaded = True
+        print("Engine is up.") 
+    
+    # use this function to search
+    def search(self, query=''):
+        if not self.index_is_loaded:
+            print('Please run() engine first!')
+            return
+        if query != '':
+            purified_q = self.__query_processor(query)
+            res = self.__search(purified_q, self.search_score_mode)
+            self.__display_results(res)
+        else:
+            q = ''
+            while q.lower() != 'exit':
+                q = input('Please Type Your Query: ')
+                purified_q = self.__query_processor(q)
+                res = self.__search(purified_q)
+                self.__display_results(res)
